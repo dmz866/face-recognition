1212214c35827d9008b8db206d93e6d246e45357
David Murillo <dmz866@hotmail.com>
2025-02-21 11:36:50 -0500

    identify face

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import face_recognition as fr
 
 pic_a = fr.load_image_file('pictures/picture_a.jpg')
 
-pic_b = fr.load_image_file('pictures/picture_b.jpg')
+pic_b = fr.load_image_file('pictures/picture_c.jpg')
 
 # Turn pictures into RGB
 pic_a = cv2.cvtColor(pic_a, cv2.COLOR_BGR2RGB)
@@ -20,6 +20,9 @@ face_b_encoded = fr.face_encodings(pic_b)[0]
 #Show Container
 cv2.rectangle(pic_a, (face_a[3],face_a[0]), (face_a[1], face_a[2]), (0, 255, 0), 2)
 cv2.rectangle(pic_b, (face_b[3],face_b[0]), (face_b[1], face_b[2]), (0, 255, 0), 2)
+
+result = fr.compare_faces([face_a_encoded], face_b_encoded)
+print(result)
 
 cv2.imshow('Picture A', pic_a)
 cv2.imshow('Picture B', pic_b)
